fbe6abb4d4cd9e8d4e956fb9fe3b6049f5fb03b3
Peter Trizuliak <petertrizuliak@Peters-MacBook-Pro-2.local>
2017-08-29 15:34:03 +0100

    add socket.io into index.js and index.html

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	res.send(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
 });
 
 http.listen(3000, function(){
